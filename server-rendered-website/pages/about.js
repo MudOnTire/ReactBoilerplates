bb6e29bd89bf52ca8545e8cb9d6e66f45286fea5
MudOnTire <895157882@qq.com>
2018-07-04 10:31:14 +0800

    modify next project

--- server-rendered-website/pages/about.js
+++ server-rendered-website/pages/about.js
@@ -1,5 +1,8 @@
+import Header from '../components/Header';
+
 const About = () => (
     <div>
+        <Header />
         <p>This is MudOnTire's About page</p>
     </div>
 )
--- server-rendered-website/pages/index.js
+++ server-rendered-website/pages/index.js
@@ -1,12 +1,9 @@
-import Link from 'next/link';
+import Layout from '../components/Layout';
 
 const Index = () => (
-    <div>
-        <Link href="/about">
-            <a>About Page</a>
-        </Link>
+    <Layout>
         <p>Hello Next.js</p>
-    </div>
+    </Layout>
 )
 
 export default Index;
