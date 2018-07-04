68ac56da4b043961be53766424cbd6e6b43006da
MudOnTire <895157882@qq.com>
2018-07-04 09:07:19 +0800

    增加next项目

---
+++ server-rendered-website/pages/about.js
@@ -0,0 +1,7 @@
+const About = () => (
+    <div>
+        <p>This is MudOnTire's About page</p>
+    </div>
+)
+
+export default About;
---
+++ server-rendered-website/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+    <div>
+        <Link href="/about">
+            <a>About Page</a>
+        </Link>
+        <p>Hello Next.js</p>
+    </div>
+)
+
+export default Index;
