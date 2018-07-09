10b8dcb5dbad231eca56e315bc6c7a036897f5a7
MudOnTire <895157882@qq.com>
2018-07-09 09:32:16 +0800

    modify next demo

--- server-rendered-website/pages/index.js
+++ server-rendered-website/pages/index.js
@@ -1,21 +1,68 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = (props) => (
-    <li>
-        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-            <a>{props.title}</a>
-        </Link>
-    </li>
-);
-
-export default () => (
+const Index = (props) => (
     <Layout>
-        <h1>My Blog</h1>
+        <h1>Marvel TV Shows</h1>
         <ul>
-            <PostLink id="hello-nextjs" title="Hello Next.js" />
-            <PostLink id="learn-nextjs" title="Next.js is awesome" />
-            <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+            {props.shows.map(({ show }) => {
+                return (
+                    <li key={show.id}>
+                        <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                            <a className="show-link">{show.name}</a>
+                        </Link>
+                    </li>
+                );
+            })}
         </ul>
+        <style jsx>
+        {`
+            *{
+                margin:0;
+                padding:0;
+            }
+            h1,a{
+                font-family:'Arial';
+            }
+            h1{
+                margin-top:20px;
+                background-color:#EF141F;
+                color:#fff;
+                font-size:50px;
+                line-height:66px;
+                text-transform: uppercase;
+                text-align:center;
+            }    
+            ul{
+                margin-top:20px;
+                padding:20px;
+                background-color:#000;
+            }
+            li{
+                list-style:none;
+                margin:5px 0;
+            }
+            a{
+                text-decoration:none;
+                color:#B4B5B4;
+                font-size:24px;
+            }
+            a:hover{
+                opacity:0.6;
+            }
+        `}
+        </style>
     </Layout>
 );
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=marvel');
+    const data = await res.json();
+    console.log(`Show data fetched. Count: ${data.length}`);
+    return {
+        shows: data
+    }
+}
+
+export default Index;
--- server-rendered-website/pages/post.js
+++ server-rendered-website/pages/post.js
@@ -1,11 +1,19 @@
-import { withRouter } from 'next/router';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
-const Post = withRouter((props) => (
+const Post = (props) => (
     <Layout>
-        <h1>{props.router.query.title}</h1>
-        <p>This is the blog post content.</p>
+        <h1>{props.show.name}</h1>
+        <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+        <img src={props.show.image.medium} />
     </Layout>
-));
+);
+
+Post.getInitialProps = async function (context) {
+    const { id } = context.query;
+    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+    const show = await res.json();
+    return { show };
+}
 
 export default Post;
